8fdfddf735acbf68e443b30ed67a0b3d117e6665
Andrew Lyric <talyric@gmail.com>
2013-06-07 11:52:47 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bunny::Application.config.secret_token = '153964595ee13c5354c7cc63c57b05d6657f565012f69d41622b57716fbf8cf69575b7e1e85d66085fcab6c496cdb4e6d32f3485e6914f841e332c1185aabd9c'
